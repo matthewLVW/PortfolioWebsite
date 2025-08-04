2a8ab0fd639f0c8b1017faa94020904d6c9c8603
Matthew Van Winkle <matthewvanwinkle@Matthews-MacBook-Pro.local>
2025-08-04 02:51:47 -0400

    hehehaha

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,9 @@ export default function Navbar() {
     <nav className="w-full p-4 border-b border-gray-200 dark:border-gray-800 flex justify-between items-center">
       <h2 className="text-xl font-semibold">Matthew VW</h2>
       <div className="space-x-6">
-        <a href="/" className="hover:underline">Home</a>
-        <a href="/projects" className="hover:underline">Projects</a>
-        <a href="/resume" className="hover:underline">Resume</a>
+        <link href="/" className="hover:underline">Home</link>
+        <link href="/projects" className="hover:underline">Projects</link>
+        <link href="/resume" className="hover:underline">Resume</link>
       </div>
     </nav>
   );
